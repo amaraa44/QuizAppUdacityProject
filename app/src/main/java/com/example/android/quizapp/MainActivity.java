8b70518867454faf7050c5ec5ea64559158aef8d
amaraa44 <halma.david.88@gmail.com>
2017-11-20 15:54:30 +0100

    I finished the Java

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.quizapp;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +15,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int numberOfGoodAnswers = 0;
-    int numberOfWrongAnswers = 0;
-
-
     public void SubmitAnswerBtn(View view) {
+        displayResult(checkAnswers());
+    }
+    private int checkAnswers(){
+        int goodAnswers = 0;
+        RadioButton q1Answer = (RadioButton) findViewById(R.id.question1_Jumpman_answer_RadioButton);
+        RadioButton q2Answer = (RadioButton) findViewById(R.id.question2_Animus_answer_RadioButton);
+        RadioButton q3Answer = (RadioButton) findViewById(R.id.question3_Link_answer_RadioButton);
+        RadioButton q4Answer = (RadioButton) findViewById(R.id.question4_Playstation2_RadioButton);
 
-
-
+        if (q1Answer.isChecked()){
+            goodAnswers = goodAnswers + 1;
+        }if (q2Answer.isChecked()) {
+            goodAnswers = goodAnswers + 1;
+        }if (q3Answer.isChecked()){
+            goodAnswers = goodAnswers + 1;
+        }if (q4Answer.isChecked()){
+            goodAnswers = goodAnswers + 1;
+        }
+        return goodAnswers;
+    }
+    private void displayResult(int numberOfGoodAnswers){
+        Toast.makeText(this, "Number of good answers: " + numberOfGoodAnswers, Toast.LENGTH_LONG).show();
     }
 }
